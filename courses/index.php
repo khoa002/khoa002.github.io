32080b66cda50aff215cb3dc2e84d15f87ff6a0d
Khoa Nguyen <khoa002@gmail.com>
2016-10-10 12:05:08 -0700

    oops

--- courses/index.php
+++ courses/index.php
@@ -1,9 +1,8 @@
 <?php
 function pg_connection_string_from_database_url() {
-    extract(parse_url("$_ENV["DATABASE_URL"]"));
-    return "user=$user password=$pass host=$host dbname=" . substr($path, 1);
+    extract(parse_url($_ENV["DATABASE_URL"]));
+    return "user=$user password=$pass host=$host sslmode=require dbname=" . substr($path, 1);
 }
-die(var_dump(pg_connection_string_from_database_url()));
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 $result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
 print "<pre>\n";
