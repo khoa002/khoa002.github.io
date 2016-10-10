1f05ac345e78e645ccac1f755c7f803a6cad7fb4
Khoa Nguyen <khoa002@gmail.com>
2016-10-10 11:59:37 -0700

    test

--- courses/index.php
+++ courses/index.php
@@ -1,7 +1,7 @@
 <?php
 function pg_connection_string_from_database_url() {
     extract(parse_url("$_ENV["DATABASE_URL"]"));
-    return "user=$user password=$pass host=$host sslmode=require dbname=" . substr($path, 1);
+    return "user=$user password=$pass host=$host dbname=" . substr($path, 1);
 }
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 $result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
