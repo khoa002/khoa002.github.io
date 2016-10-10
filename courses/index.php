3cfba9b74a33d2eb4f5adbd6a2e91d36cf3e9957
Khoa Nguyen <khoa002@gmail.com>
2016-10-10 11:49:46 -0700

    testing heroku db connection

--- courses/index.php
+++ courses/index.php
@@ -1,7 +1,7 @@
 <?php
 function pg_connection_string_from_database_url() {
     extract(parse_url($_ENV["DATABASE_URL"]));
-    return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
+    return "user=$user password=$pass host=$host dbname=" . substr($path, 1) . "sslmode=require";
 }
 die(var_dump(pg_connection_string_from_database_url()));
 ?>
