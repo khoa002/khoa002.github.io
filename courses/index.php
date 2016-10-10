fa97afbacfa779264c58997a577acdcfc5419146
Khoa Nguyen <khoa002@gmail.com>
2016-10-10 12:32:13 -0700

    testing heroku db connection using PDO

--- courses/index.php
+++ courses/index.php
@@ -1,16 +1,24 @@
 <?php
-function pg_connection_string_from_database_url() {
-    extract(parse_url($_ENV["DATABASE_URL"]));
-    return "user=$user password=$pass host=$host sslmode=require dbname=" . substr($path, 1);
-}
-$pg_conn = pg_connect(pg_connection_string_from_database_url());
-$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
-print "<pre>\n";
-if (!pg_num_rows($result)) {
-    print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
-} else {
-    print "Tables in your database:\n";
-    while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
-}
-print "\n";
+$conn_parsed = parse_url($_ENV["DATABASE_URL"]);
+
+$dsn = "pgsql:"
+     . "host={$conn_parsed["host"]};"
+     . "dbname=" . substr($conn_parsed["path"], 1) . ";"
+     . "user={$conn_parsed["user"]};"
+     . "port={$conn_parsed["port"]};"
+     . "password={$conn_parsed["pass"]};"
+     . "sslmode=require";
+// die(var_dump($dsn));
+$db = new PDO($dsn);
+die(var_dump($db));
+// $pg_conn = pg_connect(pg_connection_string_from_database_url());
+// $result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
+// print "<pre>\n";
+// if (!pg_num_rows($result)) {
+//     print("Your connection is working, but your database is empty.\nFret not. This is expected for new apps.\n");
+// } else {
+//     print "Tables in your database:\n";
+//     while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
+// }
+// print "\n";
 ?>
