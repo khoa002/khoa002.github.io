7a75141d4d020e4e3857b67117a1c46adeaf1003
Khoa Nguyen <khoa002@gmail.com>
2016-10-10 11:45:40 -0700

    oops

--- courses/index.php
+++ courses/index.php
@@ -3,5 +3,5 @@ function pg_connection_string_from_database_url() {
     extract(parse_url($_ENV["DATABASE_URL"]));
     return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
 }
-die(var_dump(pg_connection_string_from_database_url());
+die(var_dump(pg_connection_string_from_database_url()));
 ?>
